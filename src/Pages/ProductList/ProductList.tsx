3b1d8f9dc8a61152bbc3330704c6dec9b77ad543
Nastya Shkrob <shkrobnastya@gmail.com>
2024-04-01 21:57:43 +0200

    add filters section

--- src/Pages/ProductList/ProductList.tsx
+++ src/Pages/ProductList/ProductList.tsx
@@ -2,6 +2,7 @@ import ProductTile from "../../components/ProductTile";
 import classes from "./ProductList.module.scss";
 import CircularProgress from "@mui/material/CircularProgress";
 import useFetch from "../../hooks/useFetch";
+import Filters from "../../components/Filters";
 
 const ProductList = () => {
   const {
@@ -11,19 +12,22 @@ const ProductList = () => {
   } = useFetch("http://localhost:8000/products");
 
   return (
-    <div className={classes.wrapper}>
-      {error && <div>{error}</div>}
-      {isPending && (
-        <CircularProgress
-          sx={{ width: "100px" }}
-          color="inherit"
-          className="spinner"
-        />
-      )}
-      {products &&
-        products.map((product) => (
-          <ProductTile key={product.id} product={product} />
-        ))}
+    <div className={classes.container}>
+      <Filters />
+      <div className={classes.wrapper}>
+        {error && <div>{error}</div>}
+        {isPending && (
+          <CircularProgress
+            sx={{ width: "100px" }}
+            color="inherit"
+            className="spinner"
+          />
+        )}
+        {products &&
+          products.map((product) => (
+            <ProductTile key={product.id} product={product} />
+          ))}
+      </div>
     </div>
   );
 };
